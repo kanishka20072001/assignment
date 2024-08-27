2037dcacb19221cfd62b57f8f10e76b327431e77
kanishka20072001 <154266836+kanishka20072001@users.noreply.github.com>
2024-08-27 22:36:31 +0530

    Create Button.js

---
+++ Button.js
@@ -0,0 +1,12 @@
+// src/components/Button.js
+import React from 'react';
+
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
