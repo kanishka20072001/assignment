ebab0d655a6000ec15092f8716c6d2dacc3b2270
kanishka20072001 <154266836+kanishka20072001@users.noreply.github.com>
2024-08-27 22:43:28 +0530

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,12 +1,1 @@
-// src/components/Button.js
-import React from 'react';
 
-function Button({ onClick, children }) {
-  return (
-    <button onClick={onClick}>
-      {children}
-    </button>
-  );
-}
-
-export default Button;
